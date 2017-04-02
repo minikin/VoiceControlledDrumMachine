614ec049810d1750528cd52fea940eeed6c7f1d9
minikin <djminikin@gmail.com>
2017-04-02 23:49:38 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+### Voice Controlled Drum Machine (LXR Digital DRUM SYNTH)
+
+More info soon.
---
+++ midi-clock/index.js
@@ -0,0 +1,66 @@
+const midi = require('midi');
+const readline = require('readline');
+const midiOutputs = [];
+
+// Helper to handle user input in console
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Midi -- return port number or throw error if not found
+function OutPortNumberFromName(portName) {
+    const out = new midi.output();
+    for (let i = 0; i < out.getPortCount(); i++) {
+        if (out.getPortName(i) == portName) {
+            return i;
+        }
+    }
+  throw new Error("Couldn't find MIDI port '" + portName + "'");
+}
+
+function OutputForPortNumber(portNumber) {
+    if (typeof midiOutputs[portNumber] == 'undefined') {
+        midiOutputs[portNumber] = new midi.output();
+        midiOutputs[portNumber].openPort(portNumber);
+    }
+    return midiOutputs[portNumber];
+}
+
+function sleep(delay) {
+    const start = new Date().getTime();
+    while (new Date().getTime() < start + delay);
+}
+
+function prepareMidiData(bpm, outPort, channel){
+
+  const now = Date.now();
+  const bytes = [248];
+  const msPerTick = (60 * 1000 / bpm) / 24;
+
+  if (outPort != null) {
+    for (let ticks = 1; ticks <= 24; ticks++) {
+      sleep(Math.round(msPerTick));
+      OutputForPortNumber(outPort).sendMessage(bytes);
+    }
+  } else {
+    throw new Error("Couldn't find MIDI port '" + outPort + "'");
+  }
+}
+
+// Send midi clock to midi port
+function sendMidiClock(bpm){
+  console.log(`Sending midi clock : ${bpm} BMP`);
+  rl.close();
+  while (true) {
+    prepareMidiData(bpm, OutPortNumberFromName("Sonic Potions USB MIDI"), 1);
+  }
+}
+
+rl.question('What BPM do you prefere ?', (bpm) => {
+  if (bpm <= 300){
+    sendMidiClock(bpm);
+  } else {
+    console.log("Can't do this. Try another value.");
+  }
+});
